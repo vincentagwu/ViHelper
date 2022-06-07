f8c353fa18d9ffbb889e2c464e756e453dc795f4
vincentagwu <vincentagwu@gmail.com>
2022-06-07 18:08:08 +0200

    Gitignore updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibGluZXp6eiIsImEiOiJja3p2ZDI2dW8wZ3B4MnVydXMzOWhkb2M1In0.5kQR_M9pN7J_q1-Z2ne_Jw'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibGluZXp6eiIsImEiOiJja3p2ZDI2dW8wZ3B4MnVydXMzOWhkb2M1In0.5kQR_M9pN7J_q1-Z2ne_Jw'
+  }
 };
 
 /*
